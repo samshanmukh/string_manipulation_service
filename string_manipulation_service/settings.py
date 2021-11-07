87a6c202e4c7860442cc844514ac8fa3df5fb7a8
shanmukh sain <sanmukh.s@navtech.io>
2021-11-07 17:24:18 +0530

    removed error libs.

--- string_manipulation_service/settings.py
+++ string_manipulation_service/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-# import dj_database_url
-# import django_heroku
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,7 +129,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# django_heroku.settings(locals())
-import django_heroku
-django_heroku.settings(locals())import django_heroku
-django_heroku.settings(locals())
