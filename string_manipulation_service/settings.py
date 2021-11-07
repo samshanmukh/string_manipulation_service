0d6484baa3cf9a41dac61e60c4ce8669d8af3bae
shanmukh sain <sanmukh.s@navtech.io>
2021-11-07 15:01:46 +0530

    Updated setting default DB sqlite.

--- string_manipulation_service/settings.py
+++ string_manipulation_service/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 import dj_database_url
 import django_heroku
@@ -81,13 +82,9 @@ WSGI_APPLICATION = 'string_manipulation_service.wsgi.application'
 
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'ciba',
-        }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
